59d84b2cff732d52a5ede0694ba26e4287897c81
Dev Mani Shukla <devmanishukla@Devs-MacBook-Pro.local>
2018-08-30 15:05:53 +0530

    Add GET/ todos route

--- server/server.js
+++ server/server.js
@@ -10,11 +10,21 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos',(req,res) => {
-    var todo = new Todo(req.body);
+    var todo = new Todo({
+        text: req.body.text
+    });
     todo.save().then((doc) => {
         res.send(doc);
     }, (err) => {
         res.status(400).send(err);
+    })
+});
+
+app.get('/todos',(req,res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    },(e) => {
+        res.status(400).send(e);
     })
 });
 
